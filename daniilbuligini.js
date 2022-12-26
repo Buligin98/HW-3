2dcc7af3ff7775c3957ada4c122d33ae04872f1d
Buligin98 <buligindaniil98@gmail.com>
2022-12-26 15:54:11 +0400

    Hw-3

---
+++ daniilbuligini.js
@@ -0,0 +1,60 @@
+"use strict";
+// 1 problem
+
+// function evenOdd() {
+//     const number = prompt("Enter number:");
+
+//   if (number % 2 == 0) {
+//     console.log("The number is even.");
+//   }else {
+//     console.log("The number is odd.");
+//   }
+// }
+
+// 2 problem
+// let currentUserName = prompt("Enter your name: ");
+
+// alert(`${currentUserName}`);
+
+// console.log(currentUserName);
+
+// 3 problem
+
+// evenOdd();
+
+// 4 problem
+// const enterFruit = prompt("Enter your fruit: ");
+
+// switch (enterFruit) {
+//   case "apple":
+//     alert( 'Apple Good' );
+//     break;
+//   case "plum":
+//     alert( 'Plum nice' );
+//     break;
+//   case "mango":
+//     alert( 'Mango Perfect' );
+//     break;
+//   default:
+//     alert("This fruit it's soo good" );
+// };
+// 5 problem
+// for (let i = 2; i <= 100; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+// 6 problem
+// let num;
+
+// do {
+//   num = prompt("Enter number: ", 0);
+// } while (num >= 0 && num);
+
+// 7 promlem
+// let numbers = [10,12,42,55,100,90,32,55];
+// console.log( numbers.filter(elem => elem % 5 == 0) );
+
+// let result = [10, 55, 100, 90, 55];
+
+// დანარჩენსაც გავაკეთებ, მოგვიანებით
