f307f9816d6deb8aaeff05c6fedb3bdf84a88723
Buligin98 <buligindaniil98@gmail.com>
2023-01-04 01:52:08 +0400

    firstCommit

--- daniilbuligini.js
+++ daniilbuligini.js
@@ -58,3 +58,51 @@
 // let result = [10, 55, 100, 90, 55];
 
 // დანარჩენსაც გავაკეთებ, მოგვიანებით
+
+// 8 problems
+// let names = ["Gio","Luka","Nika","Ani","Eka","Nini","Sopo"];
+
+// for (let i = 0; i < names.length; i++) {
+//     if (names[i]==="Nika") {
+//     continue;
+//     }
+//     console.log(names[i]);
+// };
+
+// 9 problems ვერ მივხვდი ვერაფერს
+
+// 10
+
+// function Admin(name) {
+//   return name.isAdmin;
+// }
+
+// const user = {
+//   isAdmin: true,
+// };
+
+// const user1 = {
+//   isAdmin: false,
+// };
+
+// console.log(Admin(user));
+// console.log(Admin(user1));
+
+// 11 problems
+
+// const numbers = [
+//   1, 4, 2, 14, 90, 13, 2, 0, 78, 199, 12, 313, 315, 789, 31, 12, 1, 1, 3467, 90,
+//   70, 34, 43, 189,
+// ];
+// const maxValue = Math.max.apply(null, numbers);
+
+// console.log(maxValue);
+// 12 problems
+// const numbers = [
+//   1, 4, 2, 14, 90, 13, 2, 0, 78, 199, 12, 313, 315, 789, 31, 12, 1, 1, 3467, 90,
+//   70, 34, 43, 189,
+// ];
+
+// const minValue = Math.min.apply(null, numbers);
+
+// console.log(minValue);
